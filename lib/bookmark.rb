d2d87e8fe34ed90cabca77fcc48dec04c5f6897a
twilliams1988 <tom@teow.co.uk>
2021-10-14 16:22:08 +0100

    Change to exec_params for SQL injection protection

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -28,7 +28,7 @@ class Bookmark
       con = PG.connect :dbname => "bookmark_manager"
     end
 
-    result = con.exec("INSERT INTO bookmarks (title, url) VALUES ('#{title}','#{url}') RETURNING id, url, title")
+    result = con.exec_params("INSERT INTO bookmarks (title, url) VALUES ($1,$2) RETURNING id, url, title;", [title, url])
     Bookmark.new(id: result[0]["id"], title: result[0]["title"], url: result[0]["url"])
   end
 end
